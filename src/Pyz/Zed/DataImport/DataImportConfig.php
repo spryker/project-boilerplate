88f42e92bbb2b43aa4772127397ca92848a1be05
mikhail <kopylets@logema.org>
2022-07-20 13:52:41 +0300

    [APPS-4751] changed full_EU.yml to full_GLOBAL.yml

--- src/Pyz/Zed/DataImport/DataImportConfig.php
+++ src/Pyz/Zed/DataImport/DataImportConfig.php
@@ -75,7 +75,7 @@ class DataImportConfig extends SprykerDataImportConfig
      */
     public function getDefaultYamlConfigPath(): ?string
     {
-        return APPLICATION_ROOT_DIR . DIRECTORY_SEPARATOR . 'data/import/local/full_EU.yml';
+        return APPLICATION_ROOT_DIR . DIRECTORY_SEPARATOR . 'data/import/local/full_GLOBAL.yml';
     }
 
     /**
